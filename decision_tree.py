6238ffaa6b3b5d8dd1f9e177ec96380267d7a225
Paulo Victor Corazza <paulovictor@live.com>
2018-10-08 00:45:14 -0300

    iniciando o split dos dados

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,6 @@
+import csv
 import math
+
 
 def entropy(matrixInstances):
     totalInstances = matrixInstances.__len__()
@@ -7,7 +9,7 @@ def entropy(matrixInstances):
         values = {}
         for instance in range(matrixInstances.__len__()):
             currentFeatureValue = matrixInstances[instance][feature]
-            if(currentFeatureValue in values):
+            if (currentFeatureValue in values):
                 oldValue = values[currentFeatureValue]
                 oldValue.append(instance)
                 values[currentFeatureValue] = oldValue
@@ -16,38 +18,56 @@ def entropy(matrixInstances):
                 list.append(instance)
                 values[currentFeatureValue] = list
         entropy = 0
-        print (values)
+        print(values)
         for currentFeatureValue in values.keys():
             labels = {}
             setSize = values[currentFeatureValue].__len__()
             currentEntropy = 0
             for instance in values[currentFeatureValue]:
-                label = matrixInstances[instance][(len(matrixInstances[0])-1)]
-                if(label in labels):
+                label = matrixInstances[instance][(len(matrixInstances[0]) - 1)]
+                if (label in labels):
                     oldValue = labels[label]
                     newValue = oldValue + 1
                     labels[label] = newValue
                 else:
                     labels[label] = 1
             for label in labels.keys():
-                currentEntropy = currentEntropy - round(((labels[label])/setSize)*math.log((labels[label])/setSize, 2),3)
-            currentEntropy = currentEntropy * (setSize/totalInstances)
+                currentEntropy = currentEntropy - round(
+                    ((labels[label]) / setSize) * math.log((labels[label]) / setSize, 2), 3)
+            currentEntropy = currentEntropy * (setSize / totalInstances)
             entropy = entropy + currentEntropy
         featuresEntropy.append(entropy)
-    print (featuresEntropy)
+    print("\n")
+    print(featuresEntropy)
     exit(0)
 
 
+# Possíveis valores para a coluna
+def get_possible_values(rows, col):
+    return set([row[col] for row in rows])
+
+
+def create_decision_tree(data):
+    node = {"att": None, "value": {}}
+    # if (data.values())
+
+
 def calculate():
-    matrixInstances = None
-    with open("data/dadosBenchmark_validacaoAlgoritmoAD.csv") as instances:
-        for instance in instances:
-            splittedInstance = instance.split(';')
-            if(matrixInstances != None):
-                currentInstance = []
-                for field in splittedInstance:
-                    currentInstance.append(field)
-                matrixInstances.append(currentInstance)
-            else:
-                matrixInstances = []
-    featuresEntropy = entropy(matrixInstances)
+    data = list(csv.reader(open("data/dadosBenchmark_validacaoAlgoritmoAD.csv", "r"), delimiter=";"))
+    header = data[0]
+    index = header.index("Tempo")
+    data.pop(0)
+    # print(data)
+
+    possible_values = get_possible_values(data, index)
+
+    # for value in possible_values:
+    #     print(value)
+
+    new_data = []
+
+    for instance in data:
+        if (instance[index] == "Ensolarado"):
+            new_data.append(instance)
+
+    print(new_data)
